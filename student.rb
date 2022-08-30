37bf10d8c25ecdda7019ede537bb2ab336da281f
bruk19 <brukteshome21@gmail.com>
2022-08-30 12:11:31 +0300

    Extend parent's constructor by adding classroom

--- student.rb
+++ student.rb
@@ -1,3 +1,9 @@
 # create a student class 
-class student
+class student < person
+    #initialize a class student
+    def initialize(name="Unknown", age, parent_permission="true", classroom)
+        super(name, age, parent_permission)
+        @classroom = classroom
+    end
+
 end
