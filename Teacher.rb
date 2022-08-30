0c901a8635897c04f5ee0785b9c63ad46177a5f2
bruk19 <brukteshome21@gmail.com>
2022-08-30 12:18:21 +0300

    Initializr a class teacher

--- Teacher.rb
+++ Teacher.rb
@@ -1,3 +1,8 @@
 # create a Teacher class
 class Teacher 
+     #initialize a class Teacher
+     def initialize(name="Unknown", age, parent_permission="true", specialization)
+        super(name, age, parent_permission)
+        @specialization = specialization
+    end
 end 
