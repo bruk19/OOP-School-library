e9803d7341eb020f9bf92272c9d9a60462cf8f17
bruk19 <brukteshome21@gmail.com>
2022-08-30 11:44:58 +0300

    Add parameters and assign to the instance variable

--- person.rb
+++ person.rb
@@ -1,4 +1,10 @@
 # create a person class 
+
+#initialize a class person
 class person
     def initialize(name="Unknown", age, parent_permission="true")
+        @id = Random.rand(1...1000)
+        @name = name 
+        @age = age 
+        @parent_permission = parent_permission
 end
