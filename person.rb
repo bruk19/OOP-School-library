13b4b43306761d77a152fe468b28c4d48c6254c3
bruk19 <brukteshome21@gmail.com>
2022-08-30 11:48:23 +0300

    Create getters for person class

--- person.rb
+++ person.rb
@@ -1,10 +1,12 @@
 # create a person class 
 
-#initialize a class person
 class person
+    #initialize a class person
+    attr_reader :id
+
     def initialize(name="Unknown", age, parent_permission="true")
         @id = Random.rand(1...1000)
         @name = name 
         @age = age 
-        @parent_permission = parent_permission
+        @parent_permission = parent
 end
