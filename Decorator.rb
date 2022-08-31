afe0c9af48890382481a60fccbfe4b6adbb019b9
bruk19 <brukteshome21@gmail.com>
2022-08-31 14:44:29 +0300

    Constructor assign a nameable object

--- Decorator.rb
+++ Decorator.rb
@@ -1,6 +1,10 @@
 # create a Decorator class 
 require_relative 'Nameable'
 
+#constructor assign a nuameable object
 class Decorator < Nameable
-    
+    def initialize(nameable)
+        @nameable = Nameable
+        super()
+    end
 end
