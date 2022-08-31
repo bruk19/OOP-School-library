aafbefd10bba3c82dcd5072df5ae8ee9fc6c7cf1
bruk19 <brukteshome21@gmail.com>
2022-08-31 16:40:46 +0300

    Add required changes

--- person.rb
+++ person.rb
@@ -1,8 +1,5 @@
 # create a person class 
 require_relative 'Nameable'
-require_relative 'CapitalizeDecorator'
-require_relative 'TrimmerDecorator'
-require_relative 'Decorator'
 
 class Person < Nameable
   #initialize a class person
@@ -34,12 +31,3 @@ class Person < Nameable
   end
 
 end
-
-#checking if managed to decorate person
-person = Person.new( name:'maximilianus', age:22)
-puts person.correct_name
-
-capitalizedPerson = CapitalizeDecorator.new(person)
-puts capitalizedPerson.correct_name
-capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-puts capitalizedTrimmedPerson.
