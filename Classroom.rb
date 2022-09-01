b5ea08cb808ad5b2787b755ca71844832b91545f
bruk19 <brukteshome21@gmail.com>
2022-09-01 13:18:28 +0300

    Add method to add student one by one

--- Classroom.rb
+++ Classroom.rb
@@ -1,8 +1,14 @@
 # create a Nameable class
 class Classroom
     attr_accessor :label
+    attr_reader :student
 
     def initialize(label)
         @label = label
+    end
+
+    #Method to add student one by one
+    def add_student(student)
+        @student.push(student)
     end
 end
