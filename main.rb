695677bcecafc5ba32dfd45a00da437180bc9491
bruk19 <brukteshome21@gmail.com>
2022-09-02 13:06:34 +0300

    Create class main

--- main.rb
+++ main.rb
@@ -1,13 +1,3 @@
-#checking if managed to decorate person
-require_relative 'Nameable'
-require_relative 'CapitalizeDecorator'
-require_relative 'TrimmerDecorator'
-require_relative 'Decorator'
-
-person = Person.new( name:'maximilianus', age:22)
-puts person.correct_name
-
-capitalizedPerson = CapitalizeDecorator.new(person)
-puts capitalizedPerson.correct_name
-capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-puts capitalizedTrimmedPerson.
+# create a Main class
+class Main
+end
