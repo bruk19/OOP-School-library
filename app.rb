b87ae5c18a40517669ebbdfaf43ac95377d3a50c
bruk19 <brukteshome21@gmail.com>
2022-09-02 12:47:40 +0300

    Impliment the Create_person method

--- app.rb
+++ app.rb
@@ -5,4 +5,20 @@ class App
         @person = []
         @rental = []
     end
+
+    #Create person method
+    def create_person
+        print "Do you want to create a student (1) or a teacher (2)? [Input the number]: "
+        choice = gets.chomp
+        case choice
+        when "1"
+            puts "Student"
+        when "2"
+            puts "Teacher"
+        else
+            puts "Please insert a valid number [1 or 2]"
+            return
+        end 
+        puts "Person created successfully"
+    end  
 end
